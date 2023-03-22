135231e5acdd8ee924044290f09121c86917fed1
Vivek Kumar <vivekkumarvk0037@gmail.com>
2023-03-22 12:28:53 +0530

    Adding Files

---
+++ blogpost.js
@@ -0,0 +1,62 @@
+const posts = [
+    {
+        image: 'img/3.png',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'VIVEK KUMAR',
+        date: '07 January',
+        readTime: '15 min read',
+    },
+    {
+        image: 'img/1n.png',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'sHIVAM KUMAR',
+        date: '07 January',
+        readTime: '10 min read',
+    },
+    {
+        image: 'img/11.svg',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'ATUL KUMAR',
+        date: '07 January',
+        readTime: '16 min read',
+    },
+    {
+        image: 'img/3.png',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'DEVANSH KAUSHIK',
+        date: '07 January',
+        readTime: '17 min read',
+    },
+    {
+        image: 'img/1n.png',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'VISHAL KUMAR',
+        date: '07 January',
+        readTime: '6 min read',
+    },
+    {
+        image: 'img/11.svg',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        link: '/blogpost.html',
+        author: 'SAURAV KUMAR',
+        date: '11 January',
+        readTime: '10 min read',
+    },
+]
+
+{/* <div class="home-article">
+    <div class="home-article-img">
+        <img src="img/11.svg" alt="article">
+    </div>
+    <div class="home-article-content font1">
+        <a href="/blogpost.html"><h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3></a>
+        
+        <span>Author Name</span>
+        <span>07 January | 6 min read</span>
+    </div>
+</div> */}
